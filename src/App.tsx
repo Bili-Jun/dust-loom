e0e0cd909960340437cfb09a629f84b00c2ee8e6
Bili-jun <jun.li0220@outlook.com>
2023-12-16 11:37:24 +0800

    feat: 加入canvas

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,15 @@
-import { createSignal } from 'solid-js'
-import solidLogo from './assets/solid.svg'
-import viteLogo from '/vite.svg'
+import { createSignal, onMount } from 'solid-js'
 import './App.css'
 
 function App() {
-  const [count, setCount] = createSignal(0)
-
+  let canvasRef: HTMLCanvasElement | ((el: HTMLCanvasElement) => void) | undefined
+  onMount(() => {
+    const ctx = (canvasRef as HTMLCanvasElement)?.getContext?.("2d") as CanvasRenderingContext2D;
+    debugger
+  })
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://solidjs.com" target="_blank">
-          <img src={solidLogo} class="logo solid" alt="Solid logo" />
-        </a>
-      </div>
-      <h1>Vite + Solid</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count()}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p class="read-the-docs">
-        Click on the Vite and Solid logos to learn more
-      </p>
+      <canvas ref={canvasRef} />
     </>
   )
 }
